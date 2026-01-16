ca50fe19e16d907b1184fe2862780ce3e6e76a14
madaa2858-arch <madaa2858@gmail.com>
2026-01-16 22:51:03 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+Name:Mohamed Abdirahman Mohamud(Leader) ID:C1220472
+Name:Sabiriin Ahmed Abdi  ID:C1220794
+Name:Abdihafid Muhudin Mohamud Id:C1220467
+Name:sabarin Abuukar Abdi ID:C1220793
+
+<?php
+include "includes/header_public.php";
+?>
+
+<div class="auth-container">
+    <h2>Online Voting System</h2>
+
+    <p style="text-align:center; margin-bottom:20px;">
+        Secure • Transparent • Easy Voting
+    </p>
+
+    <a href="login.php" class="btn btn-primary" style="width:100%; margin-bottom:10px;">
+        Login
+    </a>
+
+    <a href="register.php" class="btn btn-info" style="width:100%;">
+        Register
+    </a>
+</div>
+
+<?php include "includes/footer.php"; ?>
